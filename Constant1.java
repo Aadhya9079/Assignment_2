65334ee904f073ac1b3c4dbea7c344fe9f4709fe
Aadhya9079 <146864352+Aadhya9079@users.noreply.github.com>
2024-09-05 12:06:54 +0530

    Add files via upload

---
+++ Constant1.java
@@ -0,0 +1,13 @@
+/* This class will contain all the hard coded or the constant values.
+ *  Program Owner -> Aadhya Goyal
+ *  Date -> 04-09-2024
+ */
+
+public class Constant {
+    static String AvailableOperations = ("Available operations:\n * Append\n * CountWords\n * Replace\n * isPalindrome\n * Splice\n * Split\n * MaxRepeatingCharacter\n * Sort\n * Shift\n * Reverse");
+    static String InvalidInput = "Choose a valid Input";
+    static String shiftOperation = "You've chosen Shift Operation";
+    static String shiftIndex = "Enter the Index of Shifting";
+    static String maxRepeatingChar = "The Maximum Repeating Character is ";
+    static String sortedString = "The Sorted String is ";
+}
